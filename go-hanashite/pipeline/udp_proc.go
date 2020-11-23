c9b58c5724dcbc75b5b931a1a0648736beeea2f4
Peter Triller <peter.triller@1und1.de>
2020-11-23 18:09:56 +0100

    make protoc stuff without editing

--- go-hanashite/pipeline/udp_proc.go
+++ go-hanashite/pipeline/udp_proc.go
@@ -13,39 +13,37 @@ import (
 //https://stackoverflow.com/questions/1098897/what-is-the-largest-safe-udp-packet-size-on-the-internet for Detaiils
 const MaxUDPPacketSize = 508
 
-
 //UDPProcessor is a Pipeline step to send and receive UDP Audio Frames
 type UDPProcessor struct {
-	conn net.Conn
-	userID []byte
+	conn      net.Conn
+	userID    []byte
 	channelID []byte
-
 }
 
 //NewUDPProc creates a new UDPProcessor responsible for Sending frames to a server and receiving them.
-func NewUDPProc(addr string) (*UDPProcessor, error){
-	conn, err :=  net.Dial("udp",addr)
+func NewUDPProc(addr string) (*UDPProcessor, error) {
+	conn, err := net.Dial("udp", addr)
 	if err != nil {
 		return nil, err
 	}
 
 	return &UDPProcessor{
-		conn: conn,
-		userID: []byte("falko"),
+		conn:      conn,
+		userID:    []byte("falko"),
 		channelID: []byte("secret world"),
 	}, nil
 }
 
 //OutgoingFrameProcessor wraps the FrameProcessor function for sending
 func (u *UDPProcessor) OutgoingFrameProcessor() FrameProcessor {
-	return func(frame *AudioFrame){
+	return func(frame *AudioFrame) {
 		u.processOutgoing(frame)
 	}
 }
 
 //IncomingFrameProcessor wraps the FrameProcessor function for receiving
 func (u *UDPProcessor) IncomingFrameProcessor() FrameProcessor {
-	return func(frame *AudioFrame){
+	return func(frame *AudioFrame) {
 		u.processIncomming(frame)
 	}
 }
@@ -53,10 +51,12 @@ func (u *UDPProcessor) IncomingFrameProcessor() FrameProcessor {
 func (u *UDPProcessor) processOutgoing(frame *AudioFrame) {
 	data := serialize.HanUdpMessage{
 		UserId: u.userID,
-		AudioFrame: &serialize.AudioPacket{
-			ChannelId: u.channelID,
-			SequernceId: frame.FrameNum,
-			Data: frame.Encoded[:frame.EncBytes],
+		Msg: &serialize.HanUdpMessage_AudioFrame{
+			&serialize.AudioPacket{
+				ChannelId:   u.channelID,
+				SequernceId: frame.FrameNum,
+				Data:        frame.Encoded[:frame.EncBytes],
+			},
 		},
 	}
 	out1, err := proto.Marshal(&data)
@@ -64,8 +64,8 @@ func (u *UDPProcessor) processOutgoing(frame *AudioFrame) {
 		panicIfError(err)
 	}
 	sh := serialize.StreamHeader{
-		Magic: 0x0008a71,
-		Length:  uint32(len(out1)),
+		Magic:  0x0008a71,
+		Length: uint32(len(out1)),
 	}
 	out2, err := proto.Marshal(&sh)
 	n1, err := u.conn.Write(append(out2, out1...))
@@ -86,7 +86,6 @@ func (u *UDPProcessor) processIncomming(frame *AudioFrame) {
 	sh := &serialize.StreamHeader{}
 	ap := &serialize.HanUdpMessage{}
 
-
 	//get stream header
 	err = proto.Unmarshal(buf[:10], sh)
 	panicIfError(err)
@@ -94,10 +93,11 @@ func (u *UDPProcessor) processIncomming(frame *AudioFrame) {
 	err = proto.Unmarshal(buf[10:10+sh.GetLength()], ap)
 	panicIfError(err)
 
-	frame.Encoded = ap.AudioFrame.Data
-	frame.FrameNum = ap.AudioFrame.SequernceId
+	audioFrame := ap.GetAudioFrame()
+	frame.Encoded = audioFrame.Data
+	frame.FrameNum = audioFrame.SequernceId
 
-	fmt.Printf("%d\tReceived: %d bytes in a packet\n",frame.FrameNum,n)
+	fmt.Printf("%d\tReceived: %d bytes in a packet\n", frame.FrameNum, n)
 }
 
 //FIXME useful conversion, but not used ... move to util or discard
