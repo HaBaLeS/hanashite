e9bfe68778c41653b9350f194690b7475e864acc
falko lehmann <falko@lehmann-carpzov.de>
2020-11-29 18:02:12 +0100

    commandsystem rewritten

--- go-hanashite/cmd/go-hanashite-termiu/main.go
+++ go-hanashite/cmd/go-hanashite-termiu/main.go
@@ -1,23 +1,26 @@
-// Copyright 2017 Zack Guo <zack.y.guo@gmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT license that can
-// be found in the LICENSE file.
-
-
 package main
 
 import (
 	"fmt"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	hanashite "github.com/habales/hanashite/go"
 	"github.com/habales/hanashite/go/network"
+	"github.com/habales/hanashite/go/serialize"
 	"log"
 	"strings"
 )
 
 type UI struct {
-	history *widgets.Paragraph
 	session *network.Session
-	user string
+	cfg *hanashite.Config
+
+	nickWg *widgets.Paragraph
+	chanWg *widgets.Paragraph
+	srvWg *widgets.Paragraph
+	histWg *widgets.Paragraph
+	inputWg *widgets.Paragraph
+	uvmWg *widgets.BarChart
 }
 
 
@@ -28,47 +31,20 @@ func main() {
 	}
 	defer ui.Close()
 
-//80x24
 
-	u := &UI{
-		user: "Anon",
+	u := &UI{}
+	cfg, le := hanashite.LoadConfig()
+	if le != nil {
+		panic(le)
 	}
+	u.cfg = cfg
 
-	p2 := widgets.NewParagraph()
-	p2.Title = "Commands"
-	p2.Text = "#"
-	p2.SetRect(0, 21, 80, 24)
-	p2.BorderStyle.Fg = ui.ColorYellow
+	u.initWidgets()
+	u.render()
 
-
-	u.history = widgets.NewParagraph()
-	u.history.Title = "History"
-	u.history.Text = ""
-	u.history.SetRect(7,0,80,21)
-	u.history.BorderStyle.Fg = ui.ColorMagenta
-
-
-
-
-
-	bc := widgets.NewBarChart()
-	bc.Data = []float64{33}
-	bc.Labels = []string{"dbFS"}
-	bc.Title = "UV"
-	bc.SetRect(0, 0, 6, 21)
-	bc.BarWidth = 4
-	bc.BarColors = []ui.Color{ui.ColorRed, ui.ColorGreen}
-	bc.LabelStyles = []ui.Style{ui.NewStyle(ui.ColorBlue)}
-	bc.NumStyles = []ui.Style{ui.NewStyle(ui.ColorYellow)}
-	bc.MaxVal = 100
-
-
-
-	ui.Render( bc, p2, u.history)
-
-
-
+	//UI Loop for  The Input widget
 	uiEvents := ui.PollEvents()
+	wgt := u.inputWg.Text
 	for {
 		e := <-uiEvents
 		switch e.ID {
@@ -76,67 +52,233 @@ func main() {
 			if u.session != nil {
 				u.session.Close()
 			}
-
 			return
 		}
 		if e.Type == ui.KeyboardEvent{
-			r := []rune(p2.Text)
+			r := []rune(wgt)
 			if "<Space>" == e.ID {
-				p2.Text = p2.Text + " "
+				wgt = wgt + " "
 			} else if "<Backspace>" == e.ID {
-
 				if len(r) > 1 {
-					p2.Text =string(r[:len(r)-1])
+					wgt =string(r[:len(r)-1])
 				}
-
 			} else if "<Enter>" == e.ID {
 				if len(r) > 1 {
-					p2.Text = "#"
+					wgt = "#"
 					u.processCommand(string(r[1:]))
 				}
 			} else {
-				p2.Text = p2.Text + e.ID
+				wgt = wgt + e.ID
 			}
-
 		}
-
-		ui.Render( bc, p2, u.history)
-
+		u.inputWg.Text=wgt
+		u.render()
 	}
 }
 
-func (u *UI)processCommand(cmd string) {
-	u.writeToHistory(">>> " + cmd,"red")
-	if strings.HasPrefix(cmd, "connect ") {
-		go u.connectToServer(strings.TrimLeft(cmd, "connect"))
-	} else if strings.HasPrefix(cmd, "nick ") {
-		usr := strings.TrimLeft(cmd, "nick")
-		usr = strings.TrimSpace(usr)
-		u.user = usr
-		u.writeToHistory("Set user to: "+u.user, "green")
+type Command string
+
+const (
+	Help	Command = "help"
+	List 	Command = "list"
+	Create 	Command = "create"
+	Delete	Command = "delete"
+	Join	Command = "join"
+	Part	Command = "part"
+	Connect Command = "connect"
+	Nick	Command = "nick"
+	Server	Command = "server"
+	Port	Command = "port"
+	Save	Command = "save"
+	MyStatus Command = "status"
+	ChanStatus Command="channelstatus"
+
+)
+
+var help = `
+HELP ... Abailable Commands:
+    - /help 				#this
+    - /list 				#List channels
+    - /create <arg>			#Create channel
+    - /join <arg>			#Join Channel
+    - /part	<arg>			#Leave Channel
+    - /connect 				#connect current server
+    - /nick <arg>			#change Nickname
+    - /server <arg>			#set servername
+    - /port <arg>			#set port
+    - /save 				#save nick, server, port
+    - /status				#show status
+    - /channelstatus <arg>	#channel status
+`
+
+func (u *UI)processCommand(input string) {
+	defer func() {
+		if r := recover(); r!=nil {
+			u.writeToHistory(fmt.Sprintf("Error: %v", r),"red")
+		}
+	}()
+	if strings.HasPrefix(input, "/"){
+		//we try to parse a command
+		 split := strings.Split(input[1:]," ")
+		 cmd := split[0]
+		 arg := ""
+		 if len(split) >1 {
+		 	arg = split[1]
+		 }
+		switch  Command(cmd) {
+		case Help:
+			 u.printHelp()
+		case Connect:
+			 u.connectToServer()
+		case Nick:
+			 u.updateNick(arg)
+		case List:
+			 u.session.ListChannels()
+		case Create:
+			 u.session.CreateChannel(arg)
+		case Delete:
+			 u.session.DeleteChannel(arg)
+		case Join:
+			 u.session.JoinChannel(arg)
+		case Part:
+			 u.session.PartChannel(arg)
+		case ChanStatus:
+			 u.session.ChannelStatus(arg)
+		case MyStatus:
+			 u.session.Status()
+		case Server:
+			 u.updateServer(arg)
+		case Port:
+			 u.updatePort(arg)
+		case Save:
+			 hanashite.UpdateConfig(u.cfg)
+		default:
+			 u.writeToHistory("#\"" + input + "\" unknown command", "red")
+		}
 	} else {
-		go u.writeToHistory("<<<\"" + cmd + "\" unknown command", "yellow")
+		u.writeToHistory(input, "magenta")
 	}
+
+
+
 
 }
 
-func  (u *UI)connectToServer(arg string) {
-	arg = strings.TrimSpace(arg)
-	session, err := network.NewSession(arg)
+func  (u *UI)connectToServer() {
+	session, err := network.NewSession(u.cfg, func(err error) {
+		u.writeToHistory(">>> E " + err.Error(),"red")
+	}, func(msg *serialize.HanMessage) {
+		u.handleIncomingCommand(msg)
+	})
 	if err != nil {
 		u.writeToHistory(fmt.Sprintf("<<<%s", err), "yellow")
 		return
 	}
 	u.session = session
-	err = u.session.Connect(u.user)
-	u.writeToHistory("<<< Connected to " + arg, "green")
-	if err != nil {
-		u.writeToHistory(fmt.Sprintf("<<<%s", err), "yellow")
-		return
-	}
+	u.session.Connect(u.cfg.Nickname)
+	u.writeToHistory("<<< Connected to " + u.cfg.Server, "green")
 }
 
 func (u *UI)writeToHistory(log, color string) {
-	u.history.Text = fmt.Sprintf("[%s](fg:%s)\n%s",log,color, u.history.Text)
-	ui.Render(u.history)
+	u.histWg.Text = fmt.Sprintf("[%s](fg:%s)\n%s",log,color, u.histWg.Text)
+	u.render()
+}
+
+func (u *UI) render() {
+	ui.Render(u.nickWg, u.chanWg, u.srvWg, u.histWg, u.uvmWg, u.inputWg)
+}
+
+func (u *UI) initWidgets() {
+	u.nickWg = widgets.NewParagraph()
+	u.nickWg.Title = "Nick"
+	u.nickWg.Text = fmt.Sprintf("[%s](fg:green)",u.cfg.Nickname)
+	u.nickWg.SetRect(0,0,20,3)
+
+	u.chanWg = widgets.NewParagraph()
+	u.chanWg.Title = "Channel"
+	u.chanWg.Text = fmt.Sprintf("[%s](fg:green)",u.cfg.Channel)
+	u.chanWg.SetRect(21,0,40,3)
+
+	u.srvWg = widgets.NewParagraph()
+	u.srvWg.Title = "Server"
+	u.srvWg.Text = fmt.Sprintf("[%s](fg:green):[%s](fg:green)",u.cfg.Server, u.cfg.Port)
+	u.srvWg.SetRect(41,0,80,3)
+
+	u.histWg = widgets.NewParagraph()
+	u.histWg.Title = "History"
+	u.histWg.Text = help
+	u.histWg.SetRect(7,3,80,21)
+	u.histWg.BorderStyle.Fg = ui.ColorMagenta
+
+	u.inputWg = widgets.NewParagraph()
+	u.inputWg.Title = "Commands"
+	u.inputWg.Text = "#"
+	u.inputWg.SetRect(0, 21, 80, 24)
+	u.inputWg.BorderStyle.Fg = ui.ColorYellow
+
+	u.uvmWg = widgets.NewBarChart()
+	u.uvmWg.Data = []float64{33}
+	u.uvmWg.Labels = []string{"dbFS"}
+	u.uvmWg.Title = "UV"
+	u.uvmWg.SetRect(0, 3, 6, 21)
+	u.uvmWg.BarWidth = 4
+	u.uvmWg.BarColors = []ui.Color{ui.ColorRed, ui.ColorGreen}
+	u.uvmWg.LabelStyles = []ui.Style{ui.NewStyle(ui.ColorBlue)}
+	u.uvmWg.NumStyles = []ui.Style{ui.NewStyle(ui.ColorYellow)}
+	u.uvmWg.MaxVal = 100
+}
+
+func (u *UI) updateNick(nick string) {
+	enforceArg(nick)
+	u.cfg.Nickname = nick
+	u.nickWg.Text =   fmt.Sprintf("[%s](fg:green, mod:bold)",u.cfg.Nickname)
+	u.writeToHistory("Set nick to: "+u.cfg.Nickname, "yellow")
+}
+
+func (u *UI) updateServer(arg string) {
+	enforceArg(arg)
+	u.cfg.Server= arg
+	u.srvWg.Text = fmt.Sprintf("[%s](fg:green):[%s](fg:green)",u.cfg.Server, u.cfg.Port)
+	u.writeToHistory("Server address updated", "yellow")
+}
+
+func (u *UI) updatePort(arg string) {
+	enforceArg(arg)
+	u.cfg.Port= arg
+	u.srvWg.Text = fmt.Sprintf("[%s](fg:green):[%s](fg:green)",u.cfg.Server, u.cfg.Port)
+	u.writeToHistory("Server address updated", "yellow")
+}
+
+func (u *UI) handleIncomingCommand(msg *serialize.HanMessage) {
+	switch t := msg.Msg.(type) {
+	case *serialize.HanMessage_AuthResult:
+		u.handleAuthResult(t)
+	case *serialize.HanMessage_ChanLstResult:
+		u.handleChannelListResult(t)
+	default:
+		panic(fmt.Errorf("Not implemented Type: %v", t))
+	}
+}
+
+func (u *UI) handleChannelListResult(t *serialize.HanMessage_ChanLstResult) {
+	out := ""
+	for _,v := range t.ChanLstResult.Channel {
+		out += "- " +v.Name+"\n"
+	}
+	u.writeToHistory(">>> Available Channels: \n" + out, "green")
+}
+
+func (u *UI) handleAuthResult(t *serialize.HanMessage_AuthResult) {
+	u.writeToHistory(">>> Connected", "green")
+}
+
+func (u *UI) printHelp() {
+	u.writeToHistory(help, "yellow")
+}
+
+
+func enforceArg(arg string) {
+	if arg == ""{
+		panic("Argument needed")
+	}
 }
--- go-hanashite/network/network.go
+++ go-hanashite/network/network.go
@@ -1,25 +1,29 @@
 package network
 
 import (
+	"fmt"
 	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
+	hanashite "github.com/habales/hanashite/go"
 	"github.com/habales/hanashite/go/serialize"
 	"net"
 	"time"
 )
 
 type Session struct {
-	//User
-
-	//ActiveChannel
-
 	conn net.Conn
+	errCb hanashite.NetworkErrorCallback
+	cmdCb hanashite.NetworkCommendCallback
 }
 
 
 
-func NewSession(server string) (*Session, error){
+func NewSession(cfg *hanashite.Config,ecb hanashite.NetworkErrorCallback, ccb hanashite.NetworkCommendCallback) (*Session, error){
 	session :=  &Session{}
-	conn, err  :=net.DialTimeout("tcp", server, time.Second *5)
+	session.errCb = ecb
+	session.cmdCb = ccb
+	addr := fmt.Sprintf("%s:%s", cfg.Server, cfg.Port)
+	conn, err  :=net.DialTimeout("tcp",addr , time.Second *5)
 	if err != nil {
 		return nil, err
 	}
@@ -28,31 +32,50 @@ func NewSession(server string) (*Session, error){
 	return session, nil
 }
 
-func (s *Session) Connect(user string) error{
+func (s *Session) Connect(user string) {
 	ser := serialize.HanMessage{
 		Msg: &serialize.HanMessage_Auth{
 			Auth: &serialize.Auth{
 				Username: user,
 			},
 		},
+		MessageId: getMessageID(),
 	}
 	data, err := proto.Marshal(&ser)
 	if err != nil {
-		return err
+		s.errCb(err)
+		return
 	}
-	return s.send(data)
+	err = s.send(data)
+	if err != nil {
+		s.errCb(err)
+		return
+	}
 }
 
 func (s *Session) ListChannels(){
+	hm := &serialize.HanMessage{
+		Msg: &serialize.HanMessage_ChanLst{
+			ChanLst: &serialize.ChannelList{},
+		},
+		MessageId:  getMessageID(),
+	}
 
+	data, err := proto.Marshal(hm)
+	if err != nil {
+		s.errCb(err)
+	}
+	err = s.send(data)
+	if err != nil {
+		if err != nil {
+			s.errCb(err)
+		}
+	}
 }
 
-func (s *Session) JoinChannel(channelID string){
 
-}
 
 func (s *Session) send(data []byte) error {
-
 	sh, err := proto.Marshal(&serialize.StreamHeader{
 		Magic:  0x0008a71,
 		Length: uint32(len(data)),
@@ -72,18 +95,64 @@ func (s *Session) Close() {
 }
 
 func (s *Session) handleIncomming() {
-	/*buf := make([]byte, 4096)
+	buf := make([]byte, 4096)
 	for {
 		n, err := s.conn.Read(buf)
 		if err != nil {
-			s.callback(err)
+			s.errCb(err)
+			continue
 		}
 		read := buf[:n]
 
-
-	}*/
+		sh := serialize.StreamHeader{}
+		err = proto.Unmarshal(read,&sh)
+		if err != nil {
+			s.errCb(err)
+			continue
+		}
+		hm := &serialize.HanMessage{}
+		err = proto.Unmarshal(read[10:10+sh.GetLength()], hm)
+		if err != nil {
+			s.errCb(err)
+			continue
+		}
+		s.cmdCb(hm)
+	}
 }
 
-func (s *Session) callback(err error) {
+func (s *Session) CreateChannel(arg string) {
+	panic("CreateChannel not implemented")
+}
 
+func (s *Session) JoinChannel(channelID string){
+	panic("CreateChannel not implemented")
+}
+
+func (s *Session) DeleteChannel(arg string) {
+	panic("DeleteChannel not implemented")
+}
+
+func (s *Session) PartChannel(arg string) {
+	panic("PartChannel not implemented")
+}
+
+func (s *Session) ChannelStatus(arg string) {
+	panic("ChannelStatus not implemented")
+}
+
+func (s *Session) Status() {
+	panic("Status not implemented")
+}
+
+
+func getMessageID() []byte {
+	u, err  := uuid.NewRandom()
+	if err != nil {
+		panic(err)
+	}
+	b, err := u.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	return b
 }
